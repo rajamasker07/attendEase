6447763feed28531212f26ff2c1d4f46bcf8502a
raja masker <rajamasker07@gmail.com>
2026-02-17 13:44:43 +0000

    Try fixing this error: `Runtime ReferenceError: useMemo is not defined.

--- src/app/payroll/[payrollId]/page.tsx
+++ src/app/payroll/[payrollId]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { useParams } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import {
